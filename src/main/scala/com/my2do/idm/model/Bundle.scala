877af7ee045a6f8cad7c8ce28af5478df4d4c223
Warren Strange <warren.strange@gmail.com>
2011-01-08 19:30:12 -0700

    test

--- src/main/scala/com/my2do/idm/model/Bundle.scala
+++ src/main/scala/com/my2do/idm/model/Bundle.scala
@@ -1,22 +1,8 @@
 package com.my2do.idm.model
-import org.squeryl.PrimitiveTypeMode._
 import org.identityconnectors.framework.api.ConnectorKey
 import scala.reflect.BeanProperty
 
 
-object Bundle {
-  def apply(name:String, key:ConnectorKey) = {
-    new Bundle(name,null,key.getBundleName, key.getBundleVersion, key.getConnectorName)
-  }
-  def allBundles = {
-    var x:Set[Bundle] = Set()
-    inTransaction {
-      val q = from(DB.bundles) ( b => select(b))
-      q.foreach( result =>  x += result )
-    }
-    x
-   }
-}
 
 class Bundle(
   @BeanProperty var instanceName: String = "",
